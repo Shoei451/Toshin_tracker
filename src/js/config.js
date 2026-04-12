7c4924f9d42c3b106beb98ba50ee90cabeb16094
Shoei451 <okamotoshoei451@gmail.com>
2026-04-12 19:35:05 +0900

    リダイレクト先を設定

--- src/js/config.js
+++
@@ -1,7 +0,0 @@
-﻿import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-// calendar_app と同じ Supabase プロジェクト
-const SUPABASE_URL = "https://abfuanjincelcyrlswsp.supabase.co";
-const SUPABASE_KEY = "sb_publishable_uZuTU24T38xW7iAsXJIQ-g_OfuLjyjJ";
-
-export const db = createClient(SUPABASE_URL, SUPABASE_KEY);
